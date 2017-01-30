4d29e5b2a2cce7cca96837d1a2b0552bb1a705ee
Christian Jarenfors <christian.jarenfors@gmail.com>
2017-01-30 10:54:29 +0100

    More Polish

--- Test_Hemtenta_Christian_Jarenfors/UnitTestAccount.cs
+++ Test_Hemtenta_Christian_Jarenfors/UnitTestAccount.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace HemtentaTdd2017
+using HemtentaTdd2017;
+using Moq;
+using NUnit.Framework;
+namespace Test_Hemtenta_Christian_Jarenfors
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestAccount
     {
         [TestMethod]
         public void TestMethod1()
         {
+            IAccount a = new IAccount;
         }
     }
 }
---
+++ Test_Hemtenta_Christian_Jarenfors/UnitTestBlogg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HemtentaTdd2017;
+using Moq;
+using NUnit.Framework;
+namespace Test_Hemtenta_Christian_Jarenfors
+{
+    [TestClass]
+    public class UnitTestBlogg
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
